f49bfbed15672ff81f63659e00da59e66113e328
Kamil Bouhaouli <43292484+Kamilus2001@users.noreply.github.com>
2018-12-05 22:30:16 +0100

    Update basicprogram.py

--- basicprogram.py
+++ basicprogram.py
@@ -2,9 +2,9 @@ from soundcloud_playlist import Playlist
 
 
 def Main():
-    play = Playlist('D:/chromedriver/chromedriver.exe', 'https://soundcloud.com/aetherboy1/lil-peep-crybaby-aetherboy1-flip', "new_3")
+    play = Playlist('chromedriver location', 'link to your track', "name of ur sc playlist")
     play.start()
-    x = input("Zalogowales sie i stworzyles playliste? y/n: ")
+    x = input("Have you logged in and create a playlist? y/n: ")
     if x =="y":
         play.go_to_song()
         play.next_songs()
